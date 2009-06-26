d3d809565a2926ecf281520d9deea875c24fbdcc
larsga <larsga@gmail.com>
2009-06-26 13:47:23 +0000

    Final fixes to scripts for running tests and documentation of them.

--- ontopia/src/dist/tests/runtests-ontopia.bat
+++ ontopia/src/dist/tests/runtests-ontopia.bat
@@ -1,7 +1,7 @@
 @echo off
 if "%ONTOPIA_HOME%"=="" goto error
 java net.ontopia.Ontopia
-java -Dnet.ontopia.xml.Log4jSaxErrorHandler.ignoreNamespaceErrors=true "-Dnet.ontopia.test.root=%ONTOPIA_HOME%\tests\test-data" net.ontopia.test.TestRunner --loglevel=ERROR "%ONTOPIA_HOME%\tests\test-data\tests-oks.xml" ontopia
+java -Dnet.ontopia.xml.Log4jSaxErrorHandler.ignoreNamespaceErrors=true "-Dnet.ontopia.test.root=%ONTOPIA_HOME%\tests\test-data" net.ontopia.test.TestRunner --loglevel=ERROR "%ONTOPIA_HOME%\tests\test-data\tests-ontopia.xml" ontopia
 goto done
 
 :error
