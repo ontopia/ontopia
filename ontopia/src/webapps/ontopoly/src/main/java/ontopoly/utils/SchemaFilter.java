7013da26d2c13e9b1afa96fcbf2cb6158d10a254
indiapaleale <indiapaleale@gmail.com>
2009-06-29 11:59:13 +0000

    Fixed issue 36.

--- ontopia/src/webapps/ontopoly/src/main/java/ontopoly/utils/SchemaFilter.java
+++ ontopia/src/webapps/ontopoly/src/main/java/ontopoly/utils/SchemaFilter.java
@@ -6,8 +6,12 @@ package ontopoly.utils;
 import java.util.Iterator;
 
 import net.ontopia.infoset.core.LocatorIF;
+import net.ontopia.topicmaps.core.AssociationIF;
+import net.ontopia.topicmaps.core.OccurrenceIF;
 import net.ontopia.topicmaps.core.TopicIF;
+import net.ontopia.topicmaps.core.TopicNameIF;
 import net.ontopia.topicmaps.nav2.webapps.ontopoly.model.PSI;
+import net.ontopia.topicmaps.utils.TypeHierarchyUtils;
 import net.ontopia.utils.DeciderIF;
 
 /**
@@ -15,32 +19,49 @@ import net.ontopia.utils.DeciderIF;
  */
 public class SchemaFilter implements DeciderIF {
   
+  protected TypeHierarchyUtils thutils = new TypeHierarchyUtils();
+  
   public boolean ok(Object object) {
-    boolean include = true;
     if (object instanceof TopicIF) {
-      TopicIF topic = (TopicIF)object;
-      
-      // check subject identifiers first
-      Iterator it = topic.getSubjectIdentifiers().iterator();
-      while (it.hasNext()) {
-        LocatorIF psi = (LocatorIF) it.next();
+      return includeTopic((TopicIF)object);
+    } else if (object instanceof AssociationIF) {
+      return includeTopic(((AssociationIF)object).getType());
+    } else if (object instanceof OccurrenceIF) {
+      return includeTopic(((OccurrenceIF)object).getType());
+    } else if (object instanceof TopicNameIF) {
+      TopicIF type = ((TopicNameIF)object).getType();
+      return type == null ? true : includeTopic(type);
+    }
+    return true;
+  }
 
-        // There are some exceptions
-        if (PSI.ON_DESCRIPTION.equals(psi) ||
-            PSI.ON_CREATOR.equals(psi) ||
-            PSI.ON_VERSION.equals(psi) ||
-            PSI.ON_SUPERCLASS_SUBCLASS.equals(psi) ||
-            PSI.ON_SUPERCLASS.equals(psi) ||
-            PSI.ON_SUBCLASS.equals(psi))
-          return true;
-        
-        // If one PSI is an Ontopoly system PSI, then the topic should be filtered out
-        // unless if there is an exception
-        if (psi.getAddress().startsWith("http://psi.ontopia.net/ontology/"))
-          include = false;
-      }
+  protected boolean includeTopic(TopicIF topic) {
+    boolean include = true;
+    TopicIF systemTopic = topic.getTopicMap().getTopicBySubjectIdentifier(PSI.ON_SYSTEM_TOPIC);
+    
+    if (thutils.isInstanceOf(topic, systemTopic))
+      include = false;
+    
+    // check subject identifiers first
+    Iterator it = topic.getSubjectIdentifiers().iterator();
+    while (it.hasNext()) {
+      LocatorIF psi = (LocatorIF) it.next();
+
+      // There are some exceptions
+      if (PSI.ON_DESCRIPTION.equals(psi) ||
+          PSI.ON_CREATOR.equals(psi) ||
+          PSI.ON_VERSION.equals(psi) ||
+          PSI.ON_SUPERCLASS_SUBCLASS.equals(psi) ||
+          PSI.ON_SUPERCLASS.equals(psi) ||
+          PSI.ON_SUBCLASS.equals(psi))
+        return true;
+      
+      // If one PSI is an Ontopoly system PSI, then the topic should be filtered out
+      // unless if there is an exception
+      if (psi.getAddress().startsWith("http://psi.ontopia.net/ontology/"))
+        include = false;
     }
     return include;
   }
-    
+  
 }
