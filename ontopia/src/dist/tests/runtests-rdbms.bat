7d3ab1cb825ddd87194951f40aada338db5f6c01
indiapaleale <indiapaleale@gmail.com>
2009-06-29 09:06:04 +0000

    modified test scripts to use Slf4jSaxErrorHandler

--- ontopia/src/dist/tests/runtests-rdbms.bat
+++ ontopia/src/dist/tests/runtests-rdbms.bat
@@ -3,7 +3,7 @@ if "%1"=="" goto error1
 if "%ONTOPIA_HOME%"=="" goto error2
 
 java net.ontopia.Ontopia
-java -Dnet.ontopia.xml.Log4jSaxErrorHandler.ignoreNamespaceErrors=true -Dnet.ontopia.topicmaps.impl.rdbms.PropertyFile=%1 "-Dnet.ontopia.test.root=%ONTOPIA_HOME%\tests\test-data" net.ontopia.test.TestRunner --loglevel=ERROR "%ONTOPIA_HOME%\tests\test-data\tests-oks.xml" ontopia-rdbms
+java -Dnet.ontopia.xml.Slf4jSaxErrorHandler.ignoreNamespaceErrors=true -Dnet.ontopia.topicmaps.impl.rdbms.PropertyFile=%1 "-Dnet.ontopia.test.root=%ONTOPIA_HOME%\tests\test-data" net.ontopia.test.TestRunner --loglevel=ERROR "%ONTOPIA_HOME%\tests\test-data\tests-oks.xml" ontopia-rdbms
 goto done
 
 :error1
