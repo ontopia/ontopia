325038383c45d70531166dc1387debe524a453fe
larsga <larsga@gmail.com>
2009-07-08 13:36:02 +0000

    Rewrote so that it works when the message files are loaded from .jar files, too.

--- ontopia/src/java/net/ontopia/topicmaps/viz/test/ResourceBundlesTest.java
+++ ontopia/src/java/net/ontopia/topicmaps/viz/test/ResourceBundlesTest.java
@@ -3,17 +3,15 @@
 
 package net.ontopia.topicmaps.viz.test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FilenameFilter;
+import java.io.InputStream;
 import java.io.IOException;
-import java.net.URL;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Properties;
 
-import net.ontopia.utils.StringUtils;
 import net.ontopia.test.AbstractOntopiaTestCase;
+import net.ontopia.utils.StringUtils;
+import net.ontopia.topicmaps.viz.VizController;
 
 public class ResourceBundlesTest extends AbstractOntopiaTestCase {
 
@@ -27,31 +25,23 @@ public class ResourceBundlesTest extends AbstractOntopiaTestCase {
    * the classpath.
    */
   public void testTranslationsAreConsistent() throws IOException {
-    URL msgurl = getClass().getResource("/net/ontopia/topicmaps/viz");
-    File msgdir = new File(msgurl.getFile());
-    if (!msgdir.canRead() || !msgdir.isDirectory()) {
-      fail("not a readable directory: " + msgdir);
-    }
+    String languages[] = { "de", "no", "ja" };
     
-    Properties master = loadProperties(new File(msgdir, "messages.properties"));
-    for (File file : matchFiles(msgdir, "messages_..\\.properties")) {
-      if (!file.canRead() || !file.isFile()) {
-        fail("not a readable file: " + file);
-      }
+    Properties master = loadProperties("messages.properties");
+    for (int ix = 0; ix < languages.length; ix++) {
+      String file = "messages_" + languages[ix] + ".properties";
+      Properties trans = loadProperties(file);
       List missing = new ArrayList();
       List extra = new ArrayList();
-      Properties trans = loadProperties(file);
       
       for (Object prop : trans.keySet()) {
-        if (!master.containsKey(prop)) {
+        if (!master.containsKey(prop))
           extra.add(prop);
-        }
       }
       
       for (Object prop : master.keySet()) {
-        if (!trans.containsKey(prop)) {
+        if (!trans.containsKey(prop))
           missing.add(prop);
-        }
       }
 
       assertTrue(buildReport(file, missing, extra),
@@ -59,27 +49,19 @@ public class ResourceBundlesTest extends AbstractOntopiaTestCase {
     }
   }
 
-  private static Properties loadProperties(File file) throws IOException {
+  private Properties loadProperties(String file) throws IOException {
     Properties props = new Properties();
-    FileInputStream inputStream = new FileInputStream(file);
+    InputStream inputStream = VizController.class.getResourceAsStream(file);
     props.load(inputStream);
     inputStream.close();
     return props;
   }
 
-  private static File[] matchFiles(File msgdir, final String filePattern) {
-    return msgdir.listFiles(new FilenameFilter() {
-      public boolean accept(File dir, String name) {
-        return name.matches(filePattern);
-      }
-    });
-  }
-  
   /**
    * Builds a readable error message listing everything that's wrong.
    */
-  private String buildReport(File file, List missing, List extra) {
-    String msg = file.toString();
+  private String buildReport(String file, List missing, List extra) {
+    String msg = file;
     if (!missing.isEmpty())
       msg += " is missing: " + StringUtils.join(missing, ", ");
     if (!extra.isEmpty())
