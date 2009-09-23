ca616532510835514cb09fcf3df9e2c24c45bd5d
thomas.neidhart@spaceapplications.com <thomas.neidhart@spaceapplications.com>
2009-09-23 14:30:39 +0000

    implemented to_num function according to spec

---
+++ sandbox/toma/src/main/java/net/ontopia/topicmaps/query/toma/impl/basic/function/ToNumFunction.java
@@ -0,0 +1,38 @@
+package net.ontopia.topicmaps.query.toma.impl.basic.function;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.ontopia.topicmaps.query.core.InvalidQueryException;
+import net.ontopia.topicmaps.query.toma.impl.utils.Stringifier;
+import net.ontopia.topicmaps.query.toma.parser.AntlrWrapException;
+
+/**
+ * INTERNAL: Converts a string into a number.
+ */
+public class ToNumFunction extends AbstractSimpleFunction {
+  
+  private static Pattern pattern = Pattern.compile("^\\s*([+\\-]?[0-9]+[\\.]?[0-9]*(?:[eE][+\\-]?[0-9]+)?).*");
+  
+  public ToNumFunction() {
+    super("TO_NUM", 0);
+  }
+
+  public String evaluate(Object obj) throws InvalidQueryException {
+    String str = Stringifier.toString(obj);
+    if (str != null) {
+      Matcher m = pattern.matcher(str);
+      if (m.matches()) {
+        return m.group(1);
+      } else {
+        return "0";
+      }
+    } else {
+      return "0";
+    }
+  }
+
+  public boolean validate() throws AntlrWrapException {
+    return true;
+  }
+}
