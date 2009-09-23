b8a0d9a20d45a2fd7c9ce3464a772b7f0d32ebf0
thomas.neidhart@spaceapplications.com <thomas.neidhart@spaceapplications.com>
2009-09-23 10:37:27 +0000

    added scope support for name path element

--- sandbox/toma/src/main/java/net/ontopia/topicmaps/query/toma/impl/basic/path/NamePath.java
+++ sandbox/toma/src/main/java/net/ontopia/topicmaps/query/toma/impl/basic/path/NamePath.java
@@ -2,11 +2,14 @@ package net.ontopia.topicmaps.query.toma.impl.basic.path;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Set;
 
 import net.ontopia.topicmaps.core.TopicIF;
 import net.ontopia.topicmaps.core.TopicNameIF;
 import net.ontopia.topicmaps.query.toma.impl.basic.LocalContext;
+import net.ontopia.topicmaps.query.toma.impl.basic.ResultSet;
+import net.ontopia.topicmaps.query.toma.impl.basic.expression.PathExpression;
 
 /**
  * INTERNAL: Name path element in an path expression. Returns all names
@@ -59,6 +62,37 @@ public class NamePath extends AbstractBasicPathElement {
   @SuppressWarnings("unchecked")
   public Collection<TopicNameIF> evaluate(LocalContext context, Object input) {
     TopicIF topic = (TopicIF) input;
-    return topic.getTopicNames();
+    
+    Collection<?> validScopes = null;
+    Collection<?> validTypes = null;
+    
+    if (getScope() != null) {
+      PathExpression scope = (PathExpression) getScope();
+      ResultSet scopes = scope.evaluate(context);
+      validScopes = scopes.getValues(scopes.getColumnCount() - 1);
+    }
+    
+    if (getType() != null) {
+      PathExpression type = (PathExpression) getType();
+      ResultSet types = type.evaluate(context);
+      validTypes = types.getValues(types.getColumnCount() - 1);
+    }
+    
+    Collection<TopicNameIF> names = topic.getTopicNames();
+    if (validTypes == null && validScopes == null) {
+      return names;
+    } else {
+      Collection<TopicNameIF> result = new LinkedList<TopicNameIF>();
+      for (TopicNameIF name : names) {
+        TopicIF nameType = name.getType();
+        if (validTypes == null || validTypes.contains(nameType)) {
+          if (validScopes == null || 
+              containsAny(name.getScope(), validScopes)) {
+            result.add(name);
+          }
+        }
+      }
+      return result;
+    }
   }
 }
