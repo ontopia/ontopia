0640e5588b0e33cbcb16a7a77bb2f6d7ec2aae05
thomas.neidhart@spaceapplications.com <thomas.neidhart@spaceapplications.com>
2009-10-28 16:12:09 +0000

    Improved example toma queries with square brackets.

--- ontopia/src/webapps/omnigator/plugins/query/query-samples-toma.js
+++ ontopia/src/webapps/omnigator/plugins/query/query-samples-toma.js
@@ -8,26 +8,26 @@ function insertExample(exName) {
   if (exName == "exPuccini") { // -------------------------------------------
     document.queryform.query.value =
       'select $OPERA where \n' +
-      '  puccini.(composer)<-(composed-by)->(work) = $OPERA;';
+      '  exists puccini.(composer)<-(composed-by)->(work)[$OPERA];';
 
   } else if (exName == "exPucciniSorted") { // ------------------------------
     document.queryform.query.value =
       'select $OPERA where \n' +
-      '  puccini.(composer)<-(composed-by)->(work) = $OPERA \n' +
+      '  exists puccini.(composer)<-(composed-by)->(work)[$OPERA] \n' +
       '  order by 1;';
 
   } else if (exName == "exShakespeare") { // --------------------------------
     document.queryform.query.value =
       'select $OPERA, $COMPOSER, $WORK where \n' +
-      '  shakespeare.(writer)<-(written-by)->(work) = $WORK and \n' + 
-      '  $WORK.(source)<-(based-on)->(result) = $OPERA and \n' + 
-      '  $OPERA.(work)<-(composed-by)->(composer) = $COMPOSER \n' +
-      '  order by 2;';
+      '  exists shakespeare.(writer)<-(written-by)->(work)[$WORK] \n' + 
+      '  .(source)<-(based-on)->(result)[$OPERA] \n' + 
+      '  .(work)<-(composed-by)->(composer)[$COMPOSER] \n' +
+      'order by 2;';
 
   } else if (exName == "exBornDied") { // -----------------------------------
     document.queryform.query.value =
       'select $PLACE, $PERSON where \n' +
-      '  $PERSON.(person)<-(born-in)->(place) = $PLACE and \n' +
+      '  exists $PERSON.(person)<-(born-in)->(place)[$PLACE] and \n' +
       '  $PERSON.(person)<-(died-in)->(place) = $PLACE \n' +
       '  order by 1, 2;';
 
@@ -77,9 +77,9 @@ function insertExample(exName) {
   } else if (exName == "exSuicides") { // -----------------------------------
     document.queryform.query.value =
       'select $WORK, $SUICIDE where \n' +
-      '  $SUICIDE.(perpetrator)<-(killed-by)->(victim) = $VICTIM and \n' +
-      '  $VICTIM = $SUICIDE and \n' +
-      '  $SUICIDE.(character)<-(appears-in)->(work) = $WORK \n' +
+      '  exists $SUICIDE.(perpetrator)<-(killed-by)->(victim)[$VICTIM] \n' +
+      '  .(character)<-(appears-in)->(work)[$WORK] and \n' +
+      '  $VICTIM = $SUICIDE \n' +
       'order by 1;';
     
   } else if (exName == "exSettingsByCountry") { // --------------------------
@@ -115,9 +115,9 @@ function insertExample(exName) {
     document.queryform.query.value =
       'select distinct $COMPOSER, $WRITER where \n' +
       '  $COMPOSER = verdi and \n' +
-      '  $COMPOSER.(composer)<-(composed-by)->(work) = $OPERA and \n' +
-      '  $OPERA.(result)<-(based-on)->(source) = $WORK and \n' +
-      '  $WORK.(work)<-(written-by)->(writer) = $WRITER \n' +
+      '  exists $COMPOSER.(composer)<-(composed-by)->(work)[$OPERA] \n' +
+      '  .(result)<-(based-on)->(source)[$WORK] \n' +
+      '  .(work)<-(written-by)->(writer)[$WRITER] \n' +
       'order by 2;';
 
   } else if (exName == "exBibliography") { // -------------------------------
@@ -125,7 +125,7 @@ function insertExample(exName) {
       'select $TOPIC, $TOPIC.oc(bibref).data where \n' +
       '  exists $TOPIC.oc(bibref) \n' + 
       'union \n' +
-      'select $TOPIC, $REIFIER.oc(bibref) where \n' + 
+      'select $TOPIC, $REIFIER.oc(bibref).data where \n' + 
       '  $a.player = $TOPIC and \n' +
       '  $reifier = $a.reifier and \n' +
       '  exists $reifier.oc(bibref) \n' + 
@@ -134,22 +134,21 @@ function insertExample(exName) {
   } else if (exName == "exRecordings") { // ---------------------------------
     document.queryform.query.value =
       'select $COMPOSER, $OPERA, $RECORDING.data where \n' +
-      '  $OPERA.(work)<-(composed-by)->(composer) = $COMPOSER and \n' +
-      '  $RECORDING = $OPERA.oc(audio-recording) and \n' +
-      '  exists $RECORDING \n' +
+      '  exists $OPERA.(work)<-(composed-by)->(composer)[$COMPOSER] \n' +
+      '  and \n' +
+      '  exists $OPERA.oc(audio-recording)[$RECORDING] \n' +
       '  order by 1, 2, 3;';
       
   } else if (exName == "exNoDramatisPersonae") { // -------------------------
     document.queryform.query.value =
       '# can be improved when square brackets work \n' + 
       'select $COMPOSER, $OPERA where \n' +
-      '  $OPERA.type = opera and \n' +
-      '  $OPERA.(work)<-(composed-by)->(composer) = $COMPOSER \n' +
+      '  exists opera.instance[$OPERA] \n' +
+      '  .(work)<-(composed-by)->(composer)[$COMPOSER] \n' +
       'except \n' +
       'select $COMPOSER, $OPERA where \n' +
-      '  $OPERA.type = opera and \n' +
-      '  $OPERA.(work)<-(composed-by)->(composer) = $COMPOSER and \n' +
-      '  $OPERA.(work)<-(appears-in)->(character) = $CHAR \n' +
+      '  exists $COMPOSER.(composer)<-(composed-by)->(work)[$OPERA] \n' +
+      '  .(work)<-(appears-in)->(character)[$CHAR] \n' +
       'order by 1, 2;';
     	  
   } else if (exName == "exNoVoiceType") { // --------------------------
@@ -158,8 +157,8 @@ function insertExample(exName) {
       '  $OPERA.type = opera \n' +
       'except \n' +
       'select distinct $OPERA where \n' +
-      '  $OPERA.(work)<-(appears-in)->(character) = $CHAR and \n' + 
-      '  $CHAR.(character)<-(has-voice)->(voice-type) = $TYPE \n' +
+      '  exists $OPERA.(work)<-(appears-in)->(character)[$CHAR] and \n' + 
+      '  exists $CHAR.(character)<-(has-voice)->(voice-type)[$TYPE] \n' +
       'order by 1;';
   }
 }
