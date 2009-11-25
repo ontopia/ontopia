3f566b59944ee616cfda144c5e84defc290600bb
thomas.neidhart@spaceapplications.com <thomas.neidhart@spaceapplications.com>
2009-11-25 14:38:11 +0000

    fixed JTMEncodingSniffer according to JSON RFC rules

--- ontopia/src/java/net/ontopia/topicmaps/utils/jtm/JTMEncodingSniffer.java
+++ ontopia/src/java/net/ontopia/topicmaps/utils/jtm/JTMEncodingSniffer.java
@@ -2,6 +2,9 @@ package net.ontopia.topicmaps.utils.jtm;
 
 import java.io.IOException;
 import java.io.PushbackInputStream;
+
+import org.bouncycastle.util.Arrays;
+
 import net.ontopia.topicmaps.impl.utils.EncodingSnifferIF;
 
 /**
@@ -10,28 +13,40 @@ import net.ontopia.topicmaps.impl.utils.EncodingSnifferIF;
 public class JTMEncodingSniffer implements EncodingSnifferIF {
   
   public String guessEncoding(PushbackInputStream stream) throws IOException {
-    String encoding;
+    // http://www.ietf.org/rfc/rfc4627.txt requires that a JSON data stream
+    // has to be in UTF notation. This code checks which specific UTF format
+    // is being used.
 
-    // Look to see if there's a UTF-8 BOM (Byte Order Mark) at the
-    // start of the stream.
-    byte[] bomBuffer = new byte[3];
-    boolean foundBom = false;
-    int bytesread = stream.read(bomBuffer, 0, 3);    
-    if (bytesread == 3) {
-      // Check if bomBuffer contains the UTF-8 BOM. Casts necessary to deal
-      // with signedness issues. (Java needs unsigned byte!)
-      foundBom = (bomBuffer[0] == (byte) 0xEF &&
-                  bomBuffer[1] == (byte) 0xBB &&
-                  bomBuffer[2] == (byte) 0xBF);
-      
-      if (!foundBom)
-        stream.unread(bomBuffer, 0, 3);
-    } 
-    if (foundBom) 
-      encoding = "utf-8";
-    else
-      encoding = "iso-8859-1";
+    String encoding = "UTF8";
     
+    byte[] buffer = new byte[4];
+    int bytesread = stream.read(buffer, 0, 4);    
+    if (bytesread == 4) {
+      // check for the different UTF formats:
+      //
+      // 00 00 00 xx  UTF-32BE
+      // 00 xx 00 xx  UTF-16BE
+      // xx 00 00 00  UTF-32LE
+      // xx 00 xx 00  UTF-16LE
+      // xx xx xx xx  UTF-8
+
+      // Note: UTF32 is not supported by Java
+      // http://java.sun.com/j2se/1.5.0/docs/guide/intl/encoding.doc.html
+      
+      if (buffer[0] == (byte) 0x00 &&
+          buffer[1] != (byte) 0x00 &&
+          buffer[2] == (byte) 0x00 &&
+          buffer[3] != (byte) 0x00) {
+        encoding = "UnicodeBigUnmarked";
+      } else if (buffer[0] != (byte) 0x00 &&
+          buffer[1] == (byte) 0x00 &&
+          buffer[2] != (byte) 0x00 &&
+          buffer[3] == (byte) 0x00) {
+        encoding = "UnicodeLittleUnmarked";
+      }
+    } 
+
+    stream.unread(buffer, 0, bytesread);
     return encoding;
   }  
 }
