c440d2aa1a3a1fe0915f37632765c81e3739dd86
larsga <larsga@gmail.com>
2009-12-16 13:57:48 +0000

    Added a new test.

--- ontopia/src/java/net/ontopia/topicmaps/impl/rdbms/test/RDBMSPatternSingleTopicMapSourceTest.java
+++ ontopia/src/java/net/ontopia/topicmaps/impl/rdbms/test/RDBMSPatternSingleTopicMapSourceTest.java
@@ -1,12 +1,13 @@
 
-// $Id: RDBMSPatternSingleTopicMapSourceTest.java,v 1.1 2007/09/03 07:38:18 geir.gronmo Exp $
-
 package net.ontopia.topicmaps.impl.rdbms.test;
 
-import net.ontopia.topicmaps.impl.rdbms.RDBMSPatternSingleTopicMapSource;
+import java.io.IOException;
+import net.ontopia.topicmaps.entry.TopicMapReferenceIF;
 import net.ontopia.topicmaps.entry.test.AbstractTopicMapSourceTest;
+import net.ontopia.topicmaps.impl.rdbms.*;
 
-public class RDBMSPatternSingleTopicMapSourceTest extends AbstractTopicMapSourceTest {
+public class RDBMSPatternSingleTopicMapSourceTest
+  extends AbstractTopicMapSourceTest {
 
   public RDBMSPatternSingleTopicMapSourceTest(String name) {
     super(name);
@@ -15,6 +16,54 @@ public class RDBMSPatternSingleTopicMapSourceTest extends AbstractTopicMapSource
   // --- Test cases
 
   public void testSource() {
+    // run abstract topic map source tests
+    doAbstractTopicMapSourceTests(makeSource());
+  }
+
+  public void testRefresh() throws IOException {
+    // constants
+    final String title = "test-topic-map";
+    final String propfile = System.getProperty("net.ontopia.topicmaps.impl.rdbms.PropertyFile");
+
+    RDBMSTopicMapStore store = null;
+    RDBMSTopicMapStore store2 = null;
+    try {
+      // create a new topic map matching pattern
+      store = new RDBMSTopicMapStore(propfile);
+      ((TopicMap) store.getTopicMap()).setTitle(title);
+      store.commit(); // make sure it's in the DB for real
+      
+      // verify that correct TM is being picked up
+      RDBMSPatternSingleTopicMapSource source = makeSource();
+      source.setMatch("title");
+      source.setPattern(title);
+      TopicMapReferenceIF ref = (TopicMapReferenceIF)
+        source.getReferences().iterator().next();
+      assertTrue("reference has wrong ID: " + ref,
+                 ((RDBMSTopicMapReference) ref).getTopicMapId() == store.getLongId());
+
+      // make another topic map matching pattern
+      store2 = new RDBMSTopicMapStore(propfile);
+      ((TopicMap) store2.getTopicMap()).setTitle(title);
+      store2.commit(); // make sure it's in the DB for real
+
+      // verify that new topic map is being picked up
+      source.refresh();
+      ref = (TopicMapReferenceIF) source.getReferences().iterator().next();
+      assertTrue("reference has wrong ID: " + ref,
+                 ((RDBMSTopicMapReference) ref).getTopicMapId() == store2.getLongId());
+    } finally {
+      // clean up
+      if (store != null)
+        store.delete(true);
+      if (store2 != null)
+        store2.delete(true);
+    }
+  }
+
+  // --- Internal methods
+
+  private RDBMSPatternSingleTopicMapSource makeSource() {
     RDBMSPatternSingleTopicMapSource source = new RDBMSPatternSingleTopicMapSource();
     source.setId("foosource");
     source.setTitle("footitle");
@@ -22,9 +71,7 @@ public class RDBMSPatternSingleTopicMapSourceTest extends AbstractTopicMapSource
     source.setPropertyFile(System.getProperty("net.ontopia.topicmaps.impl.rdbms.PropertyFile"));
     source.setMatch("comments");
     source.setPattern("My comment");
-    
-    // run abstract topic map source tests
-    doAbstractTopicMapSourceTests(source);
+    return source;
   }
   
 }
