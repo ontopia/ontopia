0ade5ef5e05e526a4e9db773b1adcdc0d321f9d9
h.niederhausen <h.niederhausen@googlemail.com>
2010-03-30 15:09:07 +0000

    test update for issue 225

--- ontopia/src/java/net/ontopia/topicmaps/impl/tmapi2/test/LocatorExternalFormTest.java
+++ ontopia/src/java/net/ontopia/topicmaps/impl/tmapi2/test/LocatorExternalFormTest.java
@@ -13,24 +13,31 @@ import org.tmapi.core.TopicMap;
 import org.tmapi.core.TopicMapSystem;
 import org.tmapi.core.TopicMapSystemFactory;
 
-/**
- * @author niederhausen
- *
- */
 public class LocatorExternalFormTest extends TestCase {
 
 	public void testLocatorExternalForm() throws TMAPIException {
-		 String male = "http://www.ex%25-st.com";
+		   String male = "http://www.ex%25-st.com/";
 		   TopicMapSystemFactory factory = TopicMapSystemFactory.newInstance();
 		   TopicMapSystem sys = factory.newTopicMapSystem();
 		   TopicMap tm = sys.createTopicMap("foo:baa");
 		   Topic t = tm.createTopic();
 		   Locator l = tm.createLocator(male);
+		   
+		   assertEquals(male, l.toExternalForm());
+		   
 		   Occurrence o = t.createOccurrence(tm.createTopic(), l, tm.createTopic());
-		   try {
-		     o.locatorValue().toExternalForm();
-		   } catch (NullPointerException e) {
-		     fail("NullPointerException occurred");
-		   }
+		   String locString = o.getValue();
+		   assertEquals(male, locString);
+		   
+		   // try to create a locator form value by hand
+		   Locator l2 = tm.createLocator(locString);
+		   assertEquals(male, l2);
+		   
+		   
+		   Locator loc = o.locatorValue();
+		   assertNotNull(loc);
+		  
+		   assertEquals(male, o.locatorValue().toExternalForm());
+		  
 	}
 }
