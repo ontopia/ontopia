dcbf4745f22da0bbc611a88f66570f455d1ae2da
lars.heuer <lars.heuer@unknown.com>
2010-06-06 22:31:26 +0000

    Replaced XTM2TopicMapWriter with XTM20TopicMapWriter and XTM21TopicMapWriter

---
+++ ontopia/src/java/net/ontopia/topicmaps/xml/AbstractXTM2TopicMapWriter.java
@@ -0,0 +1,96 @@
+
+// $Id$
+
+package net.ontopia.topicmaps.xml;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+
+import net.ontopia.topicmaps.core.TopicMapIF;
+import net.ontopia.topicmaps.core.TopicMapWriterIF;
+import net.ontopia.utils.DeciderIF;
+import net.ontopia.utils.OntopiaRuntimeException;
+import net.ontopia.xml.PrettyPrinter;
+
+import org.xml.sax.DocumentHandler;
+import org.xml.sax.SAXException;
+
+/**
+ * PUBLIC: A topic map writer that can write topic maps out into the
+ * interchange syntax defined by XTM 2.0 or 2.1.
+ *
+ * @since 5.1.0
+ */
+abstract class AbstractXTM2TopicMapWriter extends XTMTopicMapWriter {
+
+  /**
+   * PUBLIC: Creates a topic map writer bound to the file given in the
+   * arguments.  The topic map will be written out in the UTF-8
+   * encoding.   
+   * @param filename The name of the file to which the topic map is to
+   * be written.
+   */ 
+  public AbstractXTM2TopicMapWriter(String filename) throws IOException {
+    this(new File(filename), "utf-8");
+  }
+  
+  /**
+   * PUBLIC: Creates a topic map writer bound to the file given in the
+   * arguments.  The topic map will be written out in the UTF-8
+   * encoding.   
+   * @param file The file object to which the topic map is to be written.
+   */
+  public AbstractXTM2TopicMapWriter(File file) throws IOException {
+    this(file, "utf-8");
+  }
+
+  /**
+   * PUBLIC: Creates a topic map writer bound to the file given in the
+   * arguments.   
+   * @param file The file object to which the topic map is to be written.
+   * @param encoding The character encoding to write the topic map in.
+   */
+  public AbstractXTM2TopicMapWriter(File file, String encoding) throws IOException {
+    super(file, encoding);
+    setVersion(getVersion());
+  }
+
+  /**
+   * PUBLIC: Creates a topic map writer bound to the output stream
+   * given in the arguments.   
+   * @param stream The output stream to which the topic map is to be
+   * written.
+   * @param encoding The character encoding to write the topic map in.
+   * @exception UnsupportedEncodingException Thrown when the character
+   * encoding is not supported by the Java environment.
+   */
+  public AbstractXTM2TopicMapWriter(OutputStream stream, String encoding)
+    throws IOException, UnsupportedEncodingException {
+    this(new OutputStreamWriter(stream, encoding), encoding);
+  }
+
+  /**
+   * PUBLIC: Creates a topic map writer bound to the Writer given in
+   * the arguments; we do <em>not</em> recommend the use of this
+   * method.
+   *
+   * @param writer The Writer to which the topic map is to be
+   * written.
+   * @param encoding The character encoding the Writer writes in.
+   * Note that this <em>must</em> be set correctly, or the XML
+   * document will not parse correctly.
+   */
+  public AbstractXTM2TopicMapWriter(Writer writer, String encoding) throws IOException {
+    super(writer, encoding);
+    setVersion(getVersion());
+  }
+
+  protected abstract Version getVersion();
+
+}
--- ontopia/src/java/net/ontopia/topicmaps/xml/XTM20TopicMapWriter.java
+++ ontopia/src/java/net/ontopia/topicmaps/xml/XTM20TopicMapWriter.java
@@ -12,31 +12,32 @@ import java.io.UnsupportedEncodingException;
 
 /**
  * PUBLIC: A topic map writer that can write topic maps out into the
- * interchange syntax defined by XTM 2.1.
+ * interchange syntax defined by XTM 2.0.
  *
- * @since 5.1.0
+ * @since 4.0.0
  */
-public final class XTM21TopicMapWriter extends AbstractXTM2TopicMapWriter {
+public final class XTM20TopicMapWriter extends AbstractXTM2TopicMapWriter {
 
-  public XTM21TopicMapWriter(String filename) throws IOException {
+  public XTM20TopicMapWriter(String filename) throws IOException {
     super(new File(filename), "utf-8");
   }
   
-  public XTM21TopicMapWriter(File file) throws IOException {
+  public XTM20TopicMapWriter(File file) throws IOException {
     super(file, "utf-8");
   }
 
-  public XTM21TopicMapWriter(File file, String encoding) throws IOException {
+  public XTM20TopicMapWriter(File file, String encoding) throws IOException {
     super(file, encoding);
   }
 
-  public XTM21TopicMapWriter(OutputStream stream, String encoding)
+  public XTM20TopicMapWriter(OutputStream stream, String encoding)
     throws IOException, UnsupportedEncodingException {
     super(new OutputStreamWriter(stream, encoding), encoding);
   }
 
   protected final XTMTopicMapWriter.Version getVersion() {
-    return XTMTopicMapWriter.Version.XTM_2_1;
+    return XTMTopicMapWriter.Version.XTM_2_0;
   }
 
 }
+ 
