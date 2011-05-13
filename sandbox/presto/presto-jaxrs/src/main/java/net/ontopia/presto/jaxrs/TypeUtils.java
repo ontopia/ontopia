2764c76ceb558d2a013410bd65a5b441bffe497d
indiapaleale <indiapaleale@gmail.com>
2011-05-13 10:11:42 +0000

    Hidden types no longer show up in available-types-tree.

--- sandbox/presto/presto-jaxrs/src/main/java/net/ontopia/presto/jaxrs/TypeUtils.java
+++ sandbox/presto/presto-jaxrs/src/main/java/net/ontopia/presto/jaxrs/TypeUtils.java
@@ -2,6 +2,7 @@ package net.ontopia.presto.jaxrs;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import javax.ws.rs.core.UriInfo;
@@ -15,32 +16,37 @@ public class TypeUtils {
   static Collection<TopicTypeTree> getAvailableTypes(UriInfo uriInfo, Collection<PrestoType> types, boolean tree) {
     Collection<TopicTypeTree> result = new ArrayList<TopicTypeTree>(); 
     for (PrestoType type : types) {
-      if (type.isHidden()) {
-        continue;
-      }
-      TopicTypeTree typeMap = new TopicTypeTree();
-      typeMap.setId(type.getId());
-      typeMap.setName(type.getName());
-
-      List<Link> links = new ArrayList<Link>();
-      if (type.isCreatable()) {
-        links.add(new Link("create-instance", Links.getCreateInstanceLinkFor(uriInfo, type)));
-      }
-
-      if (tree) {
-        Collection<TopicTypeTree> typesList = getAvailableTypes(uriInfo, type.getDirectSubTypes(), true);
-        if (!typesList.isEmpty()) {
-          typeMap.setTypes(typesList);
-        }
-      } else {
-        if (!type.getDirectSubTypes().isEmpty()) {
-          links.add(new Link("available-types-tree-lazy", uriInfo.getBaseUri() + "editor/available-types-tree-lazy/" + type.getSchemaProvider().getDatabaseId() + "/" + type.getId()));
-        }
-      }
-      typeMap.setLinks(links);
-      result.add(typeMap);
+      result.addAll(getAvailableTypes(uriInfo, type, tree));
     }
     return result;
   }
 
+  private static Collection<TopicTypeTree> getAvailableTypes(UriInfo uriInfo, PrestoType type, boolean tree) {
+      if (type.isHidden()) {
+        return getAvailableTypes(uriInfo, type.getDirectSubTypes(), true);   
+      } else {
+        TopicTypeTree typeMap = new TopicTypeTree();
+        typeMap.setId(type.getId());
+        typeMap.setName(type.getName());
+  
+        List<Link> links = new ArrayList<Link>();
+        if (type.isCreatable()) {
+          links.add(new Link("create-instance", Links.getCreateInstanceLinkFor(uriInfo, type)));
+        }
+    
+        if (tree) {
+          Collection<TopicTypeTree> typesList = getAvailableTypes(uriInfo, type.getDirectSubTypes(), true);
+          if (!typesList.isEmpty()) {
+            typeMap.setTypes(typesList);
+          }
+        } else {
+          if (!type.getDirectSubTypes().isEmpty()) {
+            links.add(new Link("available-types-tree-lazy", uriInfo.getBaseUri() + "editor/available-types-tree-lazy/" + type.getSchemaProvider().getDatabaseId() + "/" + type.getId()));
+          }
+        }
+        typeMap.setLinks(links);
+        return Collections.singleton(typeMap);
+    }
+  }
+  
 }
