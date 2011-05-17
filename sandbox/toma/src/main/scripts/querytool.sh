a82682ce833b1016a3340006a2b8d2077fc6b243
qsiebers <qsiebers@gmail.com>
2011-05-17 10:53:12 +0000

    Removed all sandbox copies located in tags

--- sandbox/toma/src/main/scripts/querytool.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-LIBDIR=./lib
-CLASSPATH=./classes$(find $LIBDIR -name *.jar -exec printf :{} ';')
-echo $CLASSPATH
-java -cp $CLASSPATH net.ontopia.topicmaps.query.toma.tools.QueryTool "$@"
