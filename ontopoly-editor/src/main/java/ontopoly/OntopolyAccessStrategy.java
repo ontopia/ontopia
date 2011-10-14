072f41116c4fe362ce479d3504e32bca57c9ff1c
qsiebers <qsiebers@gmail.com>
2011-10-14 11:02:41 +0000

    Fixed issue 152 by adding javadoc to OntopolyAccessStrategy explaining what each method's expected behavior is

--- ontopoly-editor/src/main/java/ontopoly/OntopolyAccessStrategy.java
+++ ontopoly-editor/src/main/java/ontopoly/OntopolyAccessStrategy.java
@@ -7,33 +7,104 @@ import javax.servlet.http.HttpServletRequest;
 import ontopoly.model.FieldInstance;
 import ontopoly.model.Topic;
 
-
+/**
+ * Default access strategy implementation used by OntopolyApplication.
+ * Subclasses can extend this functionality to enable authentication and/or
+ * authorization for their ontopoly instance. To activate your own
+ * implementation, create an extension of {@link OntopolyApplication} and
+ * override the {@link OntopolyApplication#newAccessStrategy()} method.
+ *
+ * @see <a href="http://code.google.com/p/ontopia/wiki/OntopolyHowTo">
+ *      Ontopia googlecode - OntopolyHowTo</a>
+ * @see OntopolyApplication
+ * @see OntopolyApplication#newAccessStrategy()
+ */
 public abstract class OntopolyAccessStrategy implements Serializable {
 
+  /**
+   * The set of Privileges that a user can have.
+   */
   public enum Privilege { EDIT, READ_ONLY, NONE };
-	
+
+  /**
+   * Indicates if this strategy is enabled. An enabled strategy will cause the
+   * login page to be used. The default implementation always returns true.
+   * @return True if the strategy is enabled, false if disabled.
+   */
   public boolean isEnabled() {
-	  return true;
+    return true;
   }
-  
+
+  /**
+   * Automatically authenticates a user based on a request made. Subclasses can
+   * implement a remember-me functionality by using this method, for example by
+   * checking the presence of a cookie. Default implementation always returns
+   * null.
+   * @param request The request made that requires automatic authentication
+   * checks
+   * @return A User object when the automated authentication has succeeded, or
+   * null.
+   */
   public User autoAuthenticate(HttpServletRequest request) {
     return null;
   }
-  
+
+  /**
+   * Attempts to authenticate a user with given name and password. Subclasses
+   * should override this method to include their user verification. The default
+   * implementation makes a new user with username as name, and ignores the
+   * provided password. Note that subclasses should not throw exceptions when
+   * authentication fails, but instead return null.
+   * @param username The username to authenticate
+   * @param password The password in plain text used by the user attempting to
+   * login
+   * @return A User object when authentication succeeded, or null
+   */
   public User authenticate(String username, String password) {
     return new User(username, false);
   }
 
+  /**
+   * Resolves the privilege of a user for the provided topic. In this context,
+   * the privileges mean:
+   * <ul><li>Privilege.EDIT: The user is allowed to change this topic</li>
+   * <li>Privilege.READ_ONLY: The user is allowed to view this topic, but not
+   * to change it.</li>
+   * <li>Privilege.NONE: The user is not allowed to view or change this topic.
+   * </li></ul>
+   * @param user The user to retrieve the privilege for
+   * @param topic The topic the user is trying to access
+   * @return The privilege of the user regarding the provided topic. Must not
+   * be null.
+   */
   public Privilege getPrivilege(User user, Topic topic) {
     return Privilege.EDIT;
   }
 
+  /**
+   * Resolves the privilege of a user for the provided field. In this context,
+   * the privileges mean:
+   * <ul><li>Privilege.EDIT: The user is allowed to change this field</li>
+   * <li>Privilege.READ_ONLY: The user is allowed to view this field, but not
+   * to change it.</li>
+   * <li>Privilege.NONE: The user is not allowed to view or change this field.
+   * </li></ul>
+   * The field privilege has precedence over the topic privilege provided by
+   * {@link #getPrivilege(User, Topic)}.
+   * @param user The user to retrieve the privilege for
+   * @param fieldInstance The fieldinstance the user is trying to access
+   * @return The privilege of the user regarding the provided fieldinstance.
+   * Must not be null.
+   */
   public Privilege getPrivilege(User user, FieldInstance fieldInstance) {
     return Privilege.EDIT;
   }
 
+  /**
+   * Returns the message displayed on the login page.
+   * @return The message displayed on the login page
+   */
   public String getSignInMessage() {
-	return "Please sign in.";
+    return "Please sign in.";
   }
-
 }
