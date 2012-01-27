af8465c83f427a47618146c589615172a927a113
qsiebers <qsiebers@gmail.com>
2012-01-27 13:07:22 +0000

    Removed testrunner from main distribution

--- ontopia-distribution-tomcat/src/main/assembly/resources/tests/runtests-ontopia.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-java net.ontopia.Ontopia
-java -Xmx200M -Dnet.ontopia.xml.Slf4jSaxErrorHandler.ignoreNamespaceErrors=true -Dnet.ontopia.test.root=`pwd`/tests/test-data net.ontopia.test.TestRunner --loglevel=ERROR `pwd`/tests/test-data/tests-ontopia.xml ontopia
--- ontopia-distribution-tomcat/src/main/assembly/resources/tests/runtests-rdbms.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -n "$1" ]
-		then
-		java net.ontopia.Ontopia
-		java -Dnet.ontopia.xml.Slf4jSaxErrorHandler.ignoreNamespaceErrors=true -Dnet.ontopia.topicmaps.impl.rdbms.PropertyFile=$1 -Dnet.ontopia.test.root=`pwd`/tests/test-data net.ontopia.test.TestRunner --loglevel=ERROR `pwd`/tests/test-data/tests-ontopia.xml ontopia-rdbms
-else
-		echo "Usage: runtests-rdbms.sh <propfile>"
-fi
