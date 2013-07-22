84ddf0060d398ab7c7a92ca39aaa982c4c5619aa
qsiebers <qsiebers@gmail.com>
2013-07-22 08:48:52 +0000

    Added license headers to ontopia-vizigator, using maven-license-plugin

--- ontopia-vizigator/src/main/java/net/ontopia/topicmaps/viz/ExtendedTGPanel.java
+++ ontopia-vizigator/src/main/java/net/ontopia/topicmaps/viz/ExtendedTGPanel.java
@@ -1,4 +1,23 @@
 /*
+ * #!
+ * Ontopia Vizigator
+ * #-
+ * Copyright (C) 2001 - 2013 The Ontopia Project
+ * #-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * !#
+ */
+/*
  * NOTE! ExtendedTGPanel is a version of TGPanel that supports using an image
  * background in the VizPanel. This class contains Java 1.4 code for reading in
  * and image, but this code has been commented out, since we go for being 1.3
