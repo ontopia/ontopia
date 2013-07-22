b2f440f8943baf0c65ff5f4cc1e4e27a6c669f1d
qsiebers <qsiebers@gmail.com>
2013-07-22 08:14:23 +0000

    Added license headers to ontopia-navigator, using maven-license-plugin

--- ontopia-navigator/src/main/java/net/ontopia/topicmaps/nav/taglibs/template/package-info.java
+++ ontopia-navigator/src/main/java/net/ontopia/topicmaps/nav/taglibs/template/package-info.java
@@ -1,3 +1,22 @@
+/*
+ * #!
+ * Ontopia Navigator
+ * #-
+ * Copyright (C) 2001 - 2013 The Ontopia Project
+ * #-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * !#
+ */
 /**
 <p>Provides templating tags which enable the separation of presentation and content, or to use the terminology of the Navigator, the Model from the View. These tags will be useful for information designers who can "put" data into named string blocks, as well as graphic designers, who are able to "get" these strings and place them into html templates.</p>
 
