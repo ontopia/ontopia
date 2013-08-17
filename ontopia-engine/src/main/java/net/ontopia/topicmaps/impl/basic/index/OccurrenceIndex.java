4b04e1df1062fc33b77c5e1bf773e5ade0da3e27
p.kruijsen <p.kruijsen@gmail.com>
2013-08-17 05:40:39 +0000

    Partial revert of r2534, removed too much

--- ontopia-engine/src/main/java/net/ontopia/topicmaps/impl/basic/index/OccurrenceIndex.java
+++ ontopia-engine/src/main/java/net/ontopia/topicmaps/impl/basic/index/OccurrenceIndex.java
@@ -22,6 +22,7 @@ package net.ontopia.topicmaps.impl.basic.index;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
