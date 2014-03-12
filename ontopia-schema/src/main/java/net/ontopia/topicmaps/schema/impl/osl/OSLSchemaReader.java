7ce58e7c9db43f82e00799d6115aafdc74d88c2a
qsiebers <qsiebers@gmail.com>
2014-03-12 22:49:24 +0000

    Moved deprecated OSL schema classes to a separate module

---
+++ ontopia-schema/src/main/java/net/ontopia/topicmaps/schema/impl/osl/OSLSchemaReader.java
@@ -0,0 +1,144 @@
+/*
+ * #!
+ * Ontopia Engine
+ * #-
+ * Copyright (C) 2001 - 2013 The Ontopia Project
+ * #-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * !#
+ */
+
+package net.ontopia.topicmaps.schema.impl.osl;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import org.xml.sax.helpers.LocatorImpl;
+import net.ontopia.utils.OntopiaRuntimeException;
+import net.ontopia.utils.URIUtils;
+import net.ontopia.xml.AbstractXMLFormatReader;
+import net.ontopia.xml.ConfiguredXMLReaderFactory;
+import net.ontopia.xml.ConfigurableEntityResolver;
+import net.ontopia.xml.EmptyInputSourceFactory;
+import net.ontopia.xml.InputSourceFactoryIF;
+import net.ontopia.infoset.impl.basic.URILocator;
+import net.ontopia.topicmaps.schema.core.SchemaIF;
+import net.ontopia.topicmaps.schema.core.SchemaReaderIF;
+import net.ontopia.topicmaps.schema.core.SchemaSyntaxException;
+import net.ontopia.utils.StreamUtils;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.XMLReader;
+
+/**
+ * PUBLIC: Reader that reads OSL schemas from their XML representation
+ * into an OSL object structure.
+ */
+public class OSLSchemaReader extends AbstractXMLFormatReader
+                             implements SchemaReaderIF {
+  
+  /**
+   * PUBLIC: Creates a reader bound to the given URI.
+   */
+  public OSLSchemaReader(String uri) throws MalformedURLException, IOException {
+    source = uri.startsWith("classpath:") 
+      ? new InputSource(StreamUtils.getInputStream(uri)) 
+      : new InputSource(uri);
+    base_address = new URILocator(uri);
+  }
+  
+  /**
+   * PUBLIC: Creates a reader bound to the given file.
+   */
+  public OSLSchemaReader(File file) {
+    try {
+      source = new InputSource(URIUtils.toURL(file).toExternalForm());
+      base_address = new URILocator(URIUtils.toURL(file).toExternalForm());
+    }
+    catch (MalformedURLException e) {
+      throw new OntopiaRuntimeException("INTERNAL ERROR: " + e);
+    }
+  }
+
+  // --- SchemaReaderIF methods
+  
+  public SchemaIF read()
+    throws IOException, SchemaSyntaxException {
+    
+    // Create new parser object
+    XMLReader parser;
+    try {
+      parser = getXMLReaderFactory().createXMLReader();
+      
+    } catch (SAXException e) {
+      throw new IOException("Problems occurred when creating SAX2 XMLReader: " + e.getMessage());
+    }
+    
+    // Create content handler
+    OSLSchemaContentHandler handler = new OSLSchemaContentHandler(getXMLReaderFactory(), base_address);
+    parser.setContentHandler(handler);
+    
+    // Parse the document
+    try {
+      parser.parse(source);
+    } catch (SAXException e) {
+      if (e.getException() instanceof IOException)
+        throw (IOException) e.getException();
+      if (e.getException() instanceof SchemaSyntaxException)
+        throw (SchemaSyntaxException) e.getException();
+
+      LocatorImpl loc = new LocatorImpl();
+      if (e instanceof SAXParseException) {
+        SAXParseException e2 = (SAXParseException) e;
+        loc.setColumnNumber(e2.getColumnNumber());
+        loc.setLineNumber(e2.getLineNumber());
+        loc.setSystemId(e2.getSystemId());
+      }
+      
+      throw new SchemaSyntaxException(e.getMessage(), loc);
+    }
+    
+    return handler.getSchema();
+  }
+
+  // --- Internal methods
+
+  protected void configureXMLReaderFactory(ConfiguredXMLReaderFactory cxrfactory) {
+    cxrfactory.setFeature("http://xml.org/sax/features/namespaces", false);
+    cxrfactory.setEntityResolver(new IgnoreSchemaDTDEntityResolver());
+  }
+
+  // --- Entity resolver
+
+  static class IgnoreSchemaDTDEntityResolver extends ConfigurableEntityResolver {
+
+    protected InputSourceFactoryIF factory;
+    
+    public IgnoreSchemaDTDEntityResolver() {
+      this(new EmptyInputSourceFactory());
+    }
+  
+    public IgnoreSchemaDTDEntityResolver(InputSourceFactoryIF factory) {
+      this.factory = factory;
+      addPublicIdSource("+//IDN ontopia.net//DTD Ontopia Schema Language (1.0)//EN", factory);
+    }
+
+    public InputSource resolveEntity (String public_id, String system_id) {
+      if (system_id != null && system_id.endsWith(".dtd"))
+        return factory.createInputSource();
+      return super.resolveEntity(public_id, system_id);
+    }
+  }
+  
+}
---
+++ ontopia-schema/src/test/java/net/ontopia/topicmaps/schema/impl/osl/SchemaWriterTest.java
@@ -0,0 +1,113 @@
+/*
+ * #!
+ * Ontopia Engine
+ * #-
+ * Copyright (C) 2001 - 2013 The Ontopia Project
+ * #-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * !#
+ */
+
+package net.ontopia.topicmaps.schema.impl.osl;
+
+import java.io.IOException;
+import net.ontopia.infoset.impl.basic.URILocator;
+import net.ontopia.topicmaps.schema.core.SchemaSyntaxException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SchemaWriterTest extends AbstractSchemaTestCase {
+  private OSLSchema schema;
+  
+  @Before
+  public void setUp() {
+    try {
+      schema = new OSLSchema(new URILocator("http://www.ontopia.net"));
+    } catch (java.net.MalformedURLException e) {
+      throw new net.ontopia.utils.OntopiaRuntimeException(e);
+    }
+  }
+  
+  // --- Test methods
+
+  @Test
+  public void testEmpty() throws IOException, SchemaSyntaxException {
+    writeSchema("out", "empty.xml", schema);
+    
+    schema = (OSLSchema) readSchema("out", "empty.xml");
+    
+    Assert.assertTrue("empty schema had non-empty rule set collection",
+               schema.getRuleSets().isEmpty());
+    Assert.assertTrue("empty schema had non-empty topic class collection",
+               schema.getTopicClasses().isEmpty());
+    Assert.assertTrue("empty schema had non-empty association class collection",
+               schema.getAssociationClasses().isEmpty());
+
+    Assert.assertTrue("rule set found in empty schema",
+               schema.getRuleSet("bongo") == null);
+    Assert.assertTrue("topic class found in empty schema",
+               schema.getTopicClass("bongo") == null);
+  }
+
+  @Test
+  public void testEmptyClass() throws IOException, SchemaSyntaxException {
+    TopicClass klass = new TopicClass(schema, "bingo");
+    TypeSpecification typespec = new TypeSpecification();
+    typespec.setClassMatcher(new SubjectIndicatorMatcher(new URILocator("http://psi.ontopia.net/bongo/#bongo")));
+    klass.setTypeSpecification(typespec);
+    schema.addTopicClass(klass);
+    writeSchema("out", "emptyclass.xml", schema);
+    
+    schema = (OSLSchema) readSchema("out", "emptyclass.xml");
+    Assert.assertTrue("schema had non-empty rule set collection",
+               schema.getRuleSets().isEmpty());
+    Assert.assertTrue("wrong number of classes in schema",
+               schema.getTopicClasses().size() == 1);
+    Assert.assertTrue("schema had non-empty association class collection",
+               schema.getAssociationClasses().isEmpty());
+    Assert.assertTrue("topic class not found",
+               schema.getTopicClass("bingo") != null);
+  }
+
+  @Test
+  public void testUnscopedOccurrence() throws IOException, SchemaSyntaxException {
+    TopicClass klass = new TopicClass(schema, "bingo");
+    TypeSpecification typespec = new TypeSpecification();
+    typespec.setClassMatcher(new SubjectIndicatorMatcher(new URILocator("http://psi.ontopia.net/bongo/#bongo")));
+    klass.setTypeSpecification(typespec);
+
+    OccurrenceConstraint oc = new OccurrenceConstraint(klass);
+    typespec = new TypeSpecification();
+    typespec.setClassMatcher(new SubjectIndicatorMatcher(new URILocator("http://psi.ontopia.net/bongo/#rongo")));
+    oc.setTypeSpecification(typespec);
+    klass.addOccurrenceConstraint(oc);    
+    schema.addTopicClass(klass);
+    
+    writeSchema("out", "unscoped-occ.xml", schema);
+    schema = (OSLSchema) readSchema("out", "unscoped-occ.xml");
+    
+    Assert.assertTrue("schema had non-empty rule set collection",
+               schema.getRuleSets().isEmpty());
+    Assert.assertTrue("wrong number of classes in schema",
+               schema.getTopicClasses().size() == 1);
+    Assert.assertTrue("schema had non-empty association class collection",
+               schema.getAssociationClasses().isEmpty());
+    Assert.assertTrue("topic class not found",
+               schema.getTopicClass("bingo") != null);
+
+    klass = schema.getTopicClass("bingo");
+    Assert.assertTrue("topic class lost occurrence constraint",
+               klass.getOccurrenceConstraints().size() == 1);
+  }
+}
