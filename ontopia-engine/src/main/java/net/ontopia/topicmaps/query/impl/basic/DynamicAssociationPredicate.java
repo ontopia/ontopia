97f21d0bce8000f9ea0ece552dd4b124e647f4c1
qsiebers <qsiebers@gmail.com>
2014-10-06 10:48:30 +0000

    Fixed manual array copy

--- ontopia-engine/src/main/java/net/ontopia/topicmaps/query/impl/basic/DynamicAssociationPredicate.java
+++ ontopia-engine/src/main/java/net/ontopia/topicmaps/query/impl/basic/DynamicAssociationPredicate.java
@@ -191,8 +191,7 @@ public class DynamicAssociationPredicate extends AbstractDynamicPredicate {
             result.increaseCapacity();
           result.last++;
         
-          for (int col = 0; col < colcount; col++) 
-            result.data[result.last][col] = data[row][col];
+          System.arraycopy(data[row], 0, result.data[result.last], 0, colcount);
           for (int colix = 0; colix < unbound_length; colix++) {
             // we may have had multiple arguments for the same unbound
             // column, so have to check whether they matched up
