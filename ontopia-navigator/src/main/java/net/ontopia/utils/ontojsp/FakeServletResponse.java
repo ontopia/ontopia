a279d8c117cdc24e408028d4ca1177c811990479
Quintin Siebers <q.siebers@mssm.nl>
2015-08-17 11:51:05 +0200

    Code improvements: imports and annotations

--- ontopia-navigator/src/main/java/net/ontopia/utils/ontojsp/FakeServletResponse.java
+++ ontopia-navigator/src/main/java/net/ontopia/utils/ontojsp/FakeServletResponse.java
@@ -23,9 +23,7 @@ package net.ontopia.utils.ontojsp;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.io.Writer;
 import java.util.Locale;
-
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
@@ -50,110 +48,140 @@ public class FakeServletResponse implements HttpServletResponse {
     this.status = 200; // we default to OK
   }
   
+  @Override
   public void flushBuffer() {
   }
   
+  @Override
   public int getBufferSize() {
     return -1;
   }
   
+  @Override
   public String getCharacterEncoding() {
     return null;
   }
   
+  @Override
   public Locale getLocale() {
     return null;
   }
   
+  @Override
   public ServletOutputStream getOutputStream() throws IOException {
     return null;
   }
   
+  @Override
   public PrintWriter getWriter() throws IOException {
     return printWriter;
   }
   
+  @Override
   public boolean isCommitted() {
     return false;
   }
   
+  @Override
   public void reset() {
   }
 
+  @Override
   public void resetBuffer() {
   }
   
+  @Override
   public void setBufferSize(int size) {
   }
   
+  @Override
   public void setContentLength(int len) {
   }
 
+  @Override
   public void setContentType(java.lang.String type) {
   }
 
+  @Override
   public void setLocale(java.util.Locale loc) {
   }
 
   // http methods
 
+  @Override
   public void addCookie(Cookie cookie) {
   }
 
+  @Override
   public void addDateHeader(String name, long date) {
   }
 
+  @Override
   public void addHeader(String name, String value) {
   }
 
+  @Override
   public void addIntHeader(String name, int value) {
   }
   
+  @Override
   public boolean containsHeader(String name) {
     return false;
   }
   
+  @Override
   public String encodeRedirectUrl(String url) {
     return url;
   }
 
+  @Override
   public String encodeRedirectURL(String url) {
     return url;
   }
   
+  @Override
   public String encodeUrl(String url) {
     return url;
   }
   
+  @Override
   public String encodeURL(String url) {
     return url;
   }
   
+  @Override
   public void sendError(int sc) {
     status = sc;
   }
   
+  @Override
   public void sendError(int sc, String msg) {
     status = sc;
     statusMessage = msg;
   }
   
+  @Override
   public void sendRedirect(String location) {
   }
   
+  @Override
   public void setDateHeader(String name, long date) {
   }
   
+  @Override
   public void setHeader(String name, String value) {
   }
   
+  @Override
   public void setIntHeader(String name, int value) {
   }
             
+  @Override
   public void setStatus(int code) {
     this.status = code;
   }
   
+  @Override
   public void setStatus(int code, String msg) {
     this.status = code;
     this.statusMessage = msg;
@@ -161,6 +189,7 @@ public class FakeServletResponse implements HttpServletResponse {
 
   // --- Extra utility methods
   
+  @Override
   public int getStatus() {
     return status;
   }
@@ -171,10 +200,12 @@ public class FakeServletResponse implements HttpServletResponse {
 
   // servlets 2.4
 
+  @Override
   public void setCharacterEncoding(String charset) {
     throw new UnsupportedOperationException();
   }
 
+  @Override
   public String getContentType() {
     throw new UnsupportedOperationException();
   }
