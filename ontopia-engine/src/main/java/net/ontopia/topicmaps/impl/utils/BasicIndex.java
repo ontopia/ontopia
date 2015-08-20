df1decf2d6641a2ad1e6ba52c7e6f9c06ecccdb2
Quintin Siebers <q.siebers@mssm.nl>
2015-08-20 16:54:45 +0200

    Code cleanup

--- ontopia-engine/src/main/java/net/ontopia/topicmaps/impl/utils/BasicIndex.java
+++ ontopia-engine/src/main/java/net/ontopia/topicmaps/impl/utils/BasicIndex.java
@@ -22,7 +22,6 @@ package net.ontopia.topicmaps.impl.utils;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import net.ontopia.topicmaps.core.index.IndexIF;
 
 /**
@@ -41,26 +40,22 @@ public abstract class BasicIndex extends AbstractIndex implements EventListenerI
   // -----------------------------------------------------------------------------
 
   public void processEvent(Object object, String event, Object new_value, Object old_value) {
-    // System.out.print("Yeah:" + event);
     if (handlers.containsKey(event)) {
-      (handlers.get(event)).processEvent(object, event, new_value, old_value);
+      handlers.get(event).processEvent(object, event, new_value, old_value);
     }
-    // System.out.println(". Done.");
   }
 
   // -----------------------------------------------------------------------------
   // Event handlers
   // -----------------------------------------------------------------------------
 
-  public abstract class EventHandler implements EventListenerIF, java.io.Serializable {
+  public abstract class EventHandler implements EventListenerIF {
     public abstract void processEvent(Object object, String event, Object new_value, Object old_value);
     protected void addEvent(Object object, String event, Object value) {
-      // if (!handlers.containsKey(event)) System.out.println("+event> " + event + "=" + value);
-      (handlers.get(event)).processEvent(object, event, value, null);
+      handlers.get(event).processEvent(object, event, value, null);
     }
     protected void removeEvent(Object object, String event, Object value) {
-      // if (!handlers.containsKey(event)) System.out.println("-event> " + event + "=" + value);
-      (handlers.get(event)).processEvent(object, event, null, value);
+      handlers.get(event).processEvent(object, event, null, value);
     }
   }
   
