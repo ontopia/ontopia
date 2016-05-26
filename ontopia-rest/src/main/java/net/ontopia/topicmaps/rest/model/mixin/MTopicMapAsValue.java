3aed9771c01537c97d6579275d25fdf2426de3f8
Quintin Siebers <q.siebers@mssm.nl>
2016-05-26 11:54:14 +0200

    Added mixin interfaces for exposing topicmap core classes as json

---
+++ ontopia-rest/src/main/java/net/ontopia/topicmaps/rest/model/mixin/MTopicMapAsValue.java
@@ -0,0 +1,32 @@
+/*
+ * #!
+ * Ontopia Rest
+ * #-
+ * Copyright (C) 2001 - 2016 The Ontopia Project
+ * #-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * !#
+ */
+package net.ontopia.topicmaps.rest.model.mixin;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+@JsonIgnoreProperties(value = {"readOnly", "topicMap", "store", "associations", "topics", "builder", "transaction", "reifier"})
+public interface MTopicMapAsValue extends MTopicMap {
+
+	@Override
+	@JsonValue
+	public String getObjectId();
+	
+}
