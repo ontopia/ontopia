61852fa580c0000a4456c22b2c0bb418a3de5676
Quintin Siebers <q.siebers@mssm.nl>
2016-12-27 16:57:52 +0100

    Removed superfluous modifiers in interface fields and methods within ontopia-db2tm

--- ontopia-db2tm/src/main/java/net/ontopia/topicmaps/db2tm/ChangelogReaderIF.java
+++ ontopia-db2tm/src/main/java/net/ontopia/topicmaps/db2tm/ChangelogReaderIF.java
@@ -32,13 +32,13 @@ public interface ChangelogReaderIF extends TupleReaderIF {
    * INTERNAL: Returns the type of change that the current tuple went
    * through.
    */
-  public ChangeType getChangeType();
+  ChangeType getChangeType();
 
   /**
    * INTERNAL: Returns the current order value found in the
    * order-column. This information is used so that one can keep track
    * of incremental updates.
    */
-  public String getOrderValue();
+  String getOrderValue();
   
 }
