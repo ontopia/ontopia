ontopia/ontopia

commit 97978e46be732090a81a6b6b88c9ae03ebe8b32d

Added missing method and field modifiers to avoid use of package-private access

ontopia-classify/src/main/java/net/ontopia/topicmaps/classify/RelativeScore.java
@@ -24,8 +24,8 @@ package net.ontopia.topicmaps.classify;
  * INTERNAL: 
  */
 public class RelativeScore implements TermAnalyzerIF {
-  TermDatabase tdb;
-  double maxScore;
+  private TermDatabase tdb;
+  private double maxScore;
   
   public void analyzeTerm(Term term) {
     term.divideScore(maxScore, "relative adjustment");

ontopia-classify/src/main/java/net/ontopia/topicmaps/classify/TopicMapAnalyzer.java
@@ -43,22 +43,22 @@ import net.ontopia.utils.OntopiaRuntimeException;
  */
 public class TopicMapAnalyzer implements TermAnalyzerIF {
 
-  TermDatabase tdb;
+  private TermDatabase tdb;
   
-  TopicMapIF topicmap;
+  private TopicMapIF topicmap;
 
-  QueryProcessorIF qp;
-  ParsedQueryIF pq_byName;
+  private QueryProcessorIF qp;
+  private ParsedQueryIF pq_byName;
   
-  Collection<TopicIF> ctypes;
-  List<TopicIF> ctypes_sorted;
-  Map<TopicIF, AssociationType> atypes;
+  private Collection<TopicIF> ctypes;
+  private List<TopicIF> ctypes_sorted;
+  private Map<TopicIF, AssociationType> atypes;
   
-  Collection<TopicIF> atopics = new HashSet<TopicIF>();
-  Map<String, Variant> smap = new HashMap<String, Variant>();
-  Map<String, Collection<TopicIF>> vtopics = new HashMap<String, Collection<TopicIF>>();
+  private Collection<TopicIF> atopics = new HashSet<TopicIF>();
+  private Map<String, Variant> smap = new HashMap<String, Variant>();
+  private Map<String, Collection<TopicIF>> vtopics = new HashMap<String, Collection<TopicIF>>();
 
-  double matchFactor = 4.0d;
+  private double matchFactor = 4.0d;
   
   public TopicMapAnalyzer(TopicMapIF topicmap) {
     this.topicmap = topicmap;

ontopia-classify/src/main/java/net/ontopia/topicmaps/classify/CharacterAnalyzer.java
@@ -29,7 +29,7 @@ import org.slf4j.LoggerFactory;
 public class CharacterAnalyzer implements TermAnalyzerIF {
 
   // Define a logging category.
-  static Logger log = LoggerFactory.getLogger(CharacterAnalyzer.class.getName());
+  private static Logger log = LoggerFactory.getLogger(CharacterAnalyzer.class.getName());
 
   private static final CharacterAnalyzer INSTANCE = new CharacterAnalyzer();
   private static double FACTOR_NO_LETTERS = 0.05d;

ontopia-classify/src/main/java/net/ontopia/topicmaps/classify/Chew.java
@@ -81,7 +81,7 @@ public class Chew {
   }
 
   private static class OptionsListener implements CmdlineOptions.ListenerIF {
-    int terms = 30;
+    private int terms = 30;
     public void processOption(char option, String value) {
       if (option == 't') terms = Integer.valueOf(value).intValue();
     }

ontopia-classify/src/main/java/net/ontopia/topicmaps/classify/CompoundAnalyzer.java
@@ -35,20 +35,20 @@ import org.slf4j.LoggerFactory;
 public class CompoundAnalyzer extends AbstractDocumentAnalyzer implements TermAnalyzerIF {
 
   // Define a logging category.
-  static Logger log = LoggerFactory.getLogger(CompoundAnalyzer.class.getName());
+  private static Logger log = LoggerFactory.getLogger(CompoundAnalyzer.class.getName());
   
-  TermDatabase tdb;
-  TermStemmerIF termStemmer;
+  private TermDatabase tdb;
+  private TermStemmerIF termStemmer;
   
-  Map<Variant, Followers> followers = new HashMap<Variant, Followers>();
+  private Map<Variant, Followers> followers = new HashMap<Variant, Followers>();
 
-  int maxLength = 3;
+  private int maxLength = 3;
 
-  double term1ScoreThreshold = 0.02d;
-  double term2ScoreThreshold = 0.02d;
-  int compositeOccsThreshold = 2;
+  private double term1ScoreThreshold = 0.02d;
+  private double term2ScoreThreshold = 0.02d;
+  private int compositeOccsThreshold = 2;
 
-  double compoundFactor = 2.0d; // 0.6d;
+  private double compoundFactor = 2.0d; // 0.6d;
   
   public CompoundAnalyzer() {
     super(1);
@@ -241,7 +241,7 @@ public class CompoundAnalyzer extends AbstractDocumentAnalyzer implements TermAn
   // --------------------------------------------------------------------------
   
   private class CompositeScoreComparator implements Comparator<Variant> {
-    Followers f;
+    private Followers f;
     CompositeScoreComparator(Followers f) {
       this.f = f;
     }    
@@ -251,9 +251,9 @@ public class CompoundAnalyzer extends AbstractDocumentAnalyzer implements TermAn
   };
   
   private class Followers {
-    TObjectIntHashMap<Variant> followers = new TObjectIntHashMap<Variant>();
-    int followedByDelimiter;
-    int totalFollowerOccurrences;
+    private TObjectIntHashMap<Variant> followers = new TObjectIntHashMap<Variant>();
+    private int followedByDelimiter;
+    private int totalFollowerOccurrences;
 
     public void addFollower(Token token, int counts) {
       if (token.getType() == Token.TYPE_VARIANT) {

ontopia-classify/src/main/java/net/ontopia/topicmaps/classify/DocumentClassifier.java
@@ -27,10 +27,10 @@ import java.util.List;
  * INTERNAL: 
  */
 public class DocumentClassifier {
-  TermDatabase tdb;
-  TermStemmerIF termStemmer;
-  List<DocumentAnalyzerIF> docAnalyzers = new ArrayList<DocumentAnalyzerIF>();
-  List<TermAnalyzerIF> termAnalyzers = new ArrayList<TermAnalyzerIF>();
+  private TermDatabase tdb;
+  private TermStemmerIF termStemmer;
+  private List<DocumentAnalyzerIF> docAnalyzers = new ArrayList<DocumentAnalyzerIF>();
+  private List<TermAnalyzerIF> termAnalyzers = new ArrayList<TermAnalyzerIF>();
 
   public DocumentClassifier(TermDatabase tdb) {
     this.tdb = tdb;

ontopia-classify/src/main/java/net/ontopia/topicmaps/classify/DocumentTokenizer.java
@@ -27,10 +27,10 @@ import java.util.List;
  * INTERNAL: 
  */
 public class DocumentTokenizer {
-  TermDatabase tdb;
-  TokenizerIF tokenizer;
-  DelimiterTrimmerIF delimiterTrimmer;
-  List<TermNormalizerIF> termNormalizers = new ArrayList<TermNormalizerIF>();
+  private TermDatabase tdb;
+  private TokenizerIF tokenizer;
+  private DelimiterTrimmerIF delimiterTrimmer;
+  private List<TermNormalizerIF> termNormalizers = new ArrayList<TermNormalizerIF>();
 
   public DocumentTokenizer(TermDatabase tdb) {
     this.tdb = tdb;

ontopia-classify/src/main/java/net/ontopia/topicmaps/classify/FormatModule.java
@@ -33,7 +33,7 @@ import org.slf4j.LoggerFactory;
 public class FormatModule implements FormatModuleIF {
 
   // Define a logging category.
-  static Logger log = LoggerFactory.getLogger(FormatModule.class.getName());
+  private static Logger log = LoggerFactory.getLogger(FormatModule.class.getName());
   
   protected List<FormatModuleIF> modules;
   protected FormatModuleIF fallout_module;

commit 2817db45a2d8f4236c906f06ff9f840d1c4ecff4

Improved code that caused new issues in 97978e46

ontopia-classify/src/main/java/net/ontopia/topicmaps/classify/RelativeScore.java
@@ -24,7 +24,6 @@ package net.ontopia.topicmaps.classify;
  * INTERNAL: 
  */
 public class RelativeScore implements TermAnalyzerIF {
-  private TermDatabase tdb;
   private double maxScore;
   
   public void analyzeTerm(Term term) {
@@ -32,11 +31,9 @@ public class RelativeScore implements TermAnalyzerIF {
   }
   
   public void startAnalysis(TermDatabase tdb) {
-    this.tdb = tdb;
     this.maxScore = tdb.getMaxScore();
   }
 
   public void endAnalysis() {
-    this.tdb = null;
   }  
 }

ontopia-classify/src/main/java/net/ontopia/topicmaps/classify/TopicMapAnalyzer.java
@@ -45,9 +45,6 @@ public class TopicMapAnalyzer implements TermAnalyzerIF {
 
   private TermDatabase tdb;
   
-  private TopicMapIF topicmap;
-
-  private QueryProcessorIF qp;
   private ParsedQueryIF pq_byName;
   
   private Collection<TopicIF> ctypes;
@@ -61,9 +58,8 @@ public class TopicMapAnalyzer implements TermAnalyzerIF {
   private double matchFactor = 4.0d;
   
   public TopicMapAnalyzer(TopicMapIF topicmap) {
-    this.topicmap = topicmap;
     try {
-      this.qp = QueryUtils.getQueryProcessor(topicmap);
+      QueryProcessorIF qp = QueryUtils.getQueryProcessor(topicmap);
       this.pq_byName = qp.parse("select $T from topic-name($T, $N), value($N, %VALUE%)?");
 
       this.ctypes = new HashSet<TopicIF>();