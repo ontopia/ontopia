c2187af2fd3f86cdb794e42459862e4449edf456
Quintin Siebers <q.siebers@mssm.nl>
2025-08-22 13:28:55 +0200

    [skip ci] Upgrade classify, contentstore, db2tm and tmpref to Jakarta

--- ontopia-contentstore/src/main/java/net/ontopia/infoset/content/ContentStoreServlet.java
+++ ontopia-contentstore/src/main/java/net/ontopia/infoset/content/ContentStoreServlet.java
@@ -21,11 +21,11 @@
 package net.ontopia.infoset.content;
 
 import java.io.IOException;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import net.ontopia.topicmaps.core.TopicMapIF;
 import net.ontopia.topicmaps.nav2.core.NavigatorApplicationIF;
 import net.ontopia.topicmaps.nav2.core.NavigatorRuntimeException;
